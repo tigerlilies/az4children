0f257b413e38642172a58e58a11c6dac77b32e7c
SATOSHI TOKUNO <RAKUMASA@HOTMAIL.COM>
2017-11-15 09:56:21 -0700

    route implement

--- routes/api/childrenprofiles.js
+++ routes/api/childrenprofiles.js
@@ -1,7 +1,34 @@
-var knex = require('../..db/knex');
+var knex = require('../../db/knex');
 var express = require('express');
+var router = express.Router();
 
 //GET ALL
+router.get('/profiles', function(req, res) {
+  knex('profiles').then(function(profiles){
+    res.send(profiles);
+  })
+})
 
+//GET ONE
+router.get('/profiles/:id', function(req, res) {
+  knex('profiles').where('id',req.params.id).then(function(profile){
+    res.send(profile);
+  })
+})
+
+//POST
+router.post('/profiles', function(req, res) {
+  res.send('add-one route')
+})
+
+//PATCH
+router.put('/profiles/:id', function(req, res) {
+  res.send('change/update-one route')
+})
+
+//DELETE
+router.delete('/profiles/:id', function(req, res) {
+  res.send('delete/remove-one route')
+})
 
 module.exports = router;
--- server.js
+++ server.js
@@ -6,6 +6,8 @@ var cors = require('cors');
 var logger = require('morgan');
 var knex = require('./db/knex');
 
+var childrenprofiles = require('./routes/api/childrenprofiles');
+
 var app = express();
 
 app.set('views', path.join(__dirname, 'views'));
@@ -16,37 +18,11 @@ app.use(cors());
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/', function(req, res) {
-  res.render('index', { title: 'DBConfig' });
-})
-//GET ALL
-app.get('/profiles', function(req, res) {
-  knex.select().from('profiles').then(function(profiles){
-    res.send(profiles)
-  })
-})
+// app.get('/', function(req, res) {
+//   res.render('index', { title: 'DBConfig' });
+// })
 
-//GET ONE
-app.get('/profiles/:id', function(req, res) {
-  res.send('get-one route')
-})
-
-//POST
-app.post('/profiles', function(req, res) {
-  res.send('add-one route')
-})
-
-//PATCH
-app.put('/profiles/:id', function(req, res) {
-  res.send('change/update-one route')
-})
-
-//DELETE
-app.delete('/profiles/:id', function(req, res) {
-  res.send('delete/remove-one route')
-})
-
-
+app.use('/', childrenprofiles);
 
 app.listen(port, function() {
 console.log("listening on port: ", port);
