4cc048828657b8d9f648c457e6fa6e3d615a7b13
lukitos <lukitos@yahoo.com>
2017-12-11 13:32:59 -0700

    Add send mail using mailgun

--- routes/api/utils.js
+++ routes/api/utils.js
@@ -1,25 +1,29 @@
-var nodemailer = require('nodemailer');
 var express = require('express');
-var router = express.Router();
+var mailgun = require('mailgun.js');
+var config = require('../../config');
 
-//SEND MAIL
-router.get('/sendmail', function(req, res) {
-  let transporter = nodemailer.createTransport('smtps://tigerliliesnp@gmail.com:thel1lies@smtp.gmail.com');
-  let mailData = {
-    from: 'lukitos200@gmail.com',
-    to: 'sue.travelagent@yahoo.com',
-    subject: 'Mentor matching app',
-    text: 'Hello world',
-    html: '<b>Hello world</b>'
-  };
-  transporter.sendMail(mailData, function(error, info){
-    if(error){
-      return console.log(error);
-    }
-    console.log('Message sent: ' + info.response);
-    console.log('status:', status);
-  });
-  res.send({ success: 1 });
-})
+// Initialize
+let router = express.Router();
+let { MAILGUN_API_KEY,
+  MAILGUN_DOMAIN,
+  MAILGUN_SENDER } = config;
+let mg = mailgun.client({
+  username: 'api',
+  key: MAILGUN_API_KEY
+});
+
+// Send mail using Mailgun
+router.post('/send', function (req, res) {
+  let { to, subject, text, html } = req.body;
+  mg.messages.create(MAILGUN_DOMAIN, {
+    from: MAILGUN_SENDER,
+    to,
+    subject,
+    text,
+    html
+  })
+  .then(msg => res.send(msg))
+  .catch(err => res.send(err));
+});
 
 module.exports = router;
