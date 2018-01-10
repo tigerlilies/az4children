d397663dcb46f0edd41bad0900d875cd4432cb13
SATOSHI TOKUNO <RAKUMASA@HOTMAIL.COM>
2018-01-10 08:45:31 -0700

    complete sign in with web token

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,16 +1,18 @@
 var knex = require('../../db/knex');
 var express = require('express');
 var router = express.Router();
-var bcrypt = require('bcrypt-nodejs');
-var salt = bcrypt.genSaltSync(5);
 var jwt = require('jwt-simple');
 var config = require('../../config');
+var bcrypt = require('bcrypt-nodejs');
+var salt = bcrypt.genSaltSync(5);
 
 
-function tokenForUser(user) {
-  const timestamp = new Date().getTime();
-  return jwt.encode({ sub: user.id, iat: timestamp }, config.secret);
-}
+
+// delete it later
+// function tokenForUser(user) {
+//   const timestamp = new Date().getTime();
+//   return jwt.encode({ sub: user.id, iat: timestamp }, config.secret);
+// }
 
 // Post users
 router.post('/signup', function(req, res, next){
@@ -23,7 +25,6 @@ router.post('/signup', function(req, res, next){
       password: password
     }).then(function(user){
       res.send(user)
-      res.json({ token: tokenForUser(user) })
     })
     // if email is used, throw an error message
     .catch(
@@ -44,15 +45,20 @@ router.post('/signin', function(req, res, next){
     if (user.length > 0) {
       //store user data
       var userData = user[0];
+
       // submitted_password come first and password from database come to second
       var isVerified = bcrypt.compareSync(submitted_password, userData.password);
 
+      //User authenticated, provide a token
+      var token = jwt.encode(userData, config.secret)
+
       if (isVerified) {
         res.json({
-          data: userData
+          userData: userData,
+          token: token
         });
       } else {
-          res.status(400).send("Incorrect Password!")
+          res.status(400).send("User entered the wrong password.")
       }
     }
 
